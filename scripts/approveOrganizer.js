28fe897cffa1433607a3d485c6c4e00480582599
Joshua04d <joshua04dcosta@gmail.com>
2026-04-28 02:53:39 +0530

    reverting so no errors

--- scripts/approveOrganizer.js
+++ scripts/approveOrganizer.js
@@ -1,8 +1,7 @@
 const hre = require("hardhat");
 
 const EVENT_MANAGER = "0x08d767b58984Ca245b1c6b1a09C4bED9084beEFD";
-// const NEW_ADMIN     = "0x382E7A5eA0C6d2DfDB77C3e464227AF45f4ECD9d";
-const NEW_ADMIN     = "0xA991B1211b4B7748b0272D70DB03933dAcF5FE30";
+const NEW_ADMIN     = "0x382E7A5eA0C6d2DfDB77C3e464227AF45f4ECD9d";
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
